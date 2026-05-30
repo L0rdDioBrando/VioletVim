73b643e009e6f8b23392ae4be2dbcee0741d53a5
bopsi <kazukiobsidian@gmail.com>
2026-05-31 00:02:43 +0500

    Commit

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,5 +2,4 @@ vim.pack.add({ {
   src = "https://github.com/kylechui/nvim-surround",
   version = vim.version.range("main"),
 } })
-require("nvim-surround").setup({
-})
+require("nvim-surround").setup({})
