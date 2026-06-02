9c808595a1ffe1ea8beb8e5ae6f1f3fcd3f12938
bopsi <kazukiobsidian@gmail.com>
2026-06-02 12:53:18 +0500

    Add custom snippet

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,4 +4,22 @@ vim.schedule(function()
     "https://github.com/rafamadriz/friendly-snippets"
   }
   require("luasnip.loaders.from_vscode").lazy_load()
+
+  require("luasnip").setup({
+    history = true,
+    delete_check_events = "TextChanged",
+    region_check_events = "InsertEnter",
+  })
+  local s = require("luasnip").snippet
+  local t = require("luasnip").text_node
+  local i = require("luasnip").insert_node
+
+  require("luasnip").add_snippets("nix", {
+    s("default", {
+      t({ "{ ... }:", "", "{" }),
+      t({ "", "  imports = [", "" }),
+      t({ "    " }), i(1),
+      t({ "", "  ];", "}" }),
+    })
+  })
 end)
