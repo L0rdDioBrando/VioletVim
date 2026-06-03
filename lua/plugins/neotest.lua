8f7a01c75ce75e674ff382a2f63c52dd1e35bd48
bopsi <kazukiobsidian@gmail.com>
2026-06-03 20:37:49 +0500

    Remove vim.shedule function and add bash support

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,14 +1,14 @@
-vim.schedule(function()
-  vim.pack.add {
-    "https://github.com/nvim-neotest/neotest",
-    "https://github.com/nvim-neotest/nvim-nio",
-    "https://github.com/nvim-lua/plenary.nvim",
-    "https://github.com/nvim-neotest/neotest-go",
-  }
+vim.pack.add {
+  "https://github.com/nvim-neotest/neotest",
+  "https://github.com/nvim-neotest/nvim-nio",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/nvim-neotest/neotest-go",
+  "https://github.com/rcasia/neotest-bash",
+}
 
-  require("neotest").setup({
-    adapters = {
-      require("neotest-go"),
-    },
-  })
-end)
+require("neotest").setup({
+  adapters = {
+    require("neotest-go"),
+    require("neotest-bash")
+  },
+})
