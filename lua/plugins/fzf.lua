4ae643c5efe2d8a3ca224527e87743fff5f24e14
bopsi <kazukiobsidian@gmail.com>
2026-06-05 13:00:14 +0500

    Global update

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,30 +1,20 @@
 vim.pack.add { "https://github.com/ibhagwan/fzf-lua", "https://github.com/nvim-tree/nvim-web-devicons" }
 
 require("fzf-lua").setup {
-  treesitter = {
-    enabled = true,
-  },
-  file_icon_padding = " ",
   winopts = {
     preview = {
       border = "rounded",
-      scrollbar = "float",
-      image_renderer = "viu",
+      scrollbar = "float"
     },
-    height = 0.85,
+    height = 0.9,
     width = 0.8,
-    row = 0.35,
+    row = 0.5,
     col = 0.5,
     border = "rounded",
-    backdrop = 60,
+    backdrop = 60
   },
   files = {
-    previewer = "builtin",
-  },
-  keymap = {},
-  actions = {},
-  defaults = {
-    previewer = true,
+    previewer = "bat"
   },
   fzf_opts = {
     ["--color"] = table.concat({
@@ -39,27 +29,21 @@ require("fzf-lua").setup {
       "pointer:" .. colors.rosewater,
       "marker:" .. colors.rosewater,
       "spinner:" .. colors.rosewater,
-      "header:" .. colors.red,
+      "header:" .. colors.red
     }, ","),
   },
-  fzf_colors = false,
   hls = {
     normal = "FzfLuaNormal",
     border = "FzfLuaBorder",
     preview_normal = "FzfLuaNormal",
     preview_border = "FzfLuaBorder",
     scrollbar_f = "PmenuSel",
-    scrollbar_e = "PmenuSbar",
+    scrollbar_e = "PmenuSbar"
   },
   previewers = {
-    bat        = {
+    bat = {
       cmd  = "bat",
-      args = "--color=always --style=numbers,changes",
+      args = "--color=always --style=numbers,changes"
     },
-    extensions = {
-      ["png"] = { "chafa", "{file}" },
-      ["svg"] = { "chafa", "{file}" },
-      ["jpg"] = { "chafa", "{file}" },
-    },
-  },
+  }
 }
