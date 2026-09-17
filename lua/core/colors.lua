00949d03934af89da34bb5194db5726748e5b85c
L0rdDioBrando <kazukiobsidian@gmail.com>
2026-09-17 18:13:57 +0500

    Changes

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -2,63 +2,6 @@ vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin"
 _G.colors = require("catppuccin.palettes").get_palette("macchiato")
 require("catppuccin").setup({
   flavour = "macchiato", -- latte, frappe, macchiato, mocha
-  background = {
-    light = "latte",
-    dark = "macchiato",
-  },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  transparent_background = false, -- disables setting the background color.
-  float = {
-    transparent = false,          -- enable transparent floating windows
-    solid = false,                -- use solid styling for floating windows, see |winborder|
-  },
-  show_end_of_buffer = false,     -- shows the "~" characters after the end of buffers
-  term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-  dim_inactive = {
-    enabled = false,              -- dims the background color of inactive window
-    shade = "dark",
-    percentage = 0.15,            -- percentage of the shade to apply to the inactive window
-  },
-  no_italic = false,              -- Force no italic
-  no_bold = false,                -- Force no bold
-  no_underline = false,           -- Force no underline
-  styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" },      -- Change the style of comments
-    conditionals = { "italic" },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = {},
-    operators = {},
-  },
-  lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
-    virtual_text = {
-      errors = { "italic" },
-      hints = { "italic" },
-      warnings = { "italic" },
-      information = { "italic" },
-      ok = { "italic" },
-    },
-    underlines = {
-      errors = { "underline" },
-      hints = { "underline" },
-      warnings = { "underline" },
-      information = { "underline" },
-      ok = { "underline" },
-    },
-    inlay_hints = {
-      background = true,
-    },
-  },
-  color_overrides = {},
   custom_highlights = {
     -- Catppuccin
     Rosewater = { fg = colors.rosewater },
@@ -119,7 +62,6 @@ require("catppuccin").setup({
     NotifyTRACETitle = { fg = colors.text },
     NotifyTRACEBody = { fg = colors.lavender, bg = colors.base },
   },
-  default_integrations = false,
   auto_integrations = true,
   integrations = {
     native_lsp = {
