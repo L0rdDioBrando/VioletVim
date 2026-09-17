eb0b00ba49365a477f5d0a52a4e2ccc9c12ed7c3
L0rdDioBrando <kazukiobsidian@gmail.com>
2026-09-17 18:34:57 +0500

    Remove WorkDir icon

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -350,7 +350,7 @@ local WorkDir = {
   },
   {
     provider = function()
-      local icon = " " .. " "
+      local icon = " "
       local cwd = vim.fn.getcwd(0)
       cwd = vim.fn.fnamemodify(cwd, ":~")
       if not conditions.width_percent_below(#cwd, 0.25) then
